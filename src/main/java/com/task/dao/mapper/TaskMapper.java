2b6848df545c9b53870371a725e20c420f7ab900
hull <12345678>
2018-09-29 22:37:15 +0800

    修改任务信息

--- src/main/java/com/task/dao/mapper/TaskMapper.java
+++ src/main/java/com/task/dao/mapper/TaskMapper.java
@@ -3,6 +3,7 @@ package com.task.dao.mapper;
 import java.util.List;
 
 import com.task.entity.Task;
+import org.apache.ibatis.annotations.Param;
 
 public interface TaskMapper {
     int deleteByPrimaryKey(String id);
@@ -14,4 +15,38 @@ public interface TaskMapper {
     List<Task> selectAll();
 
     int updateByPrimaryKey(Task record);
+
+    /**
+     * 查询7天内可以领取的任务
+     * @return
+     */
+    List<Task> selectPending(@Param("pageNum") int pageNum, @Param("size") int pageSize);
+
+    /**
+     * 查询用户20分钟之内领取的任务
+     * @param userId
+     * @return
+     */
+    List<Task> selectMinuteByUserId(String userId);
+
+    /**
+     * 查询用户下任务
+     * @param userId
+     * @return
+     */
+    List<Task> selectByUserId(String userId);
+
+    /**
+     * 查询用户当天领取多少任务
+     * @param userId
+     * @return
+     */
+    List<Task>selectByDays(String userId);
+
+    /**
+     * 查看用户未完成任务
+     * @param id
+     */
+    Task selectUncomplete(String id);
+
 }
--- src/main/java/com/task/service/ITaskService.java
+++ src/main/java/com/task/service/ITaskService.java
@@ -12,58 +12,52 @@ import com.task.entity.Task;
  * @since since
  */
 public interface ITaskService {
-//    void deleteByPrimaryKey(String id);
-//
-//    void insert(Task task);
-//
-//    void updateByPrimaryKey(Task task);
-//
-//    Task selectByPrimaryKey(String id);
-//
-//    /**
-//     * 查询7天内任务
-//     * @return
-//     */
-//    List<Task> selectPending();
-//
-//    /**
-//     * 查询20天之内领取的任务
-//     * @param userId
-//     * @return
-//     */
-//    List<Task> selectByReceiveTime(String userId);
-//
-//    /**
-//     * 查询用户下任务
-//     * @param userId
-//     * @return
-//     */
-//    List<Task> selectByUserId(String userId);
-//
-//    /**
-//     * 查询用户当前领取多少任务
-//     * @param userId
-//     * @return
-//     */
-//    List<Task>selectByDays(String userId);
-//
-//    /**
-//     * 完成任务
-//     * @param id
-//     */
-//    void complete(String id);
-//
-//    /**
-//     * 申请领取任务
-//     * @param id
-//     */
-//    void apply(String id);
-//
-//    /**
-//     * 处理任务申请
-//     * @param id
-//     * @param state
-//     */
-//    void approve(String id, int state);
 
+    void deleteByPrimaryKey(String id);
+
+    void insert(Task task);
+
+    void updateByPrimaryKey(Task task);
+
+    Task selectByPrimaryKey(String id);
+
+    /**
+     * 查询7天内可以领取的任务
+     * @return
+     */
+    List<Task> selectPending(int pageNum,int size);
+
+    /**
+     * 查询用户20分钟之内领取的任务
+     * @param userId
+     * @return
+     */
+    List<Task> selectMinuteByUserId(String userId);
+
+    /**
+     * 查询用户下任务
+     * @param userId
+     * @return
+     */
+    List<Task> selectByUserId(String userId);
+
+    /**
+     * 查询用户当前领取多少任务
+     * @param userId
+     * @return
+     */
+    List<Task>selectByDays(String userId);
+
+    /**
+     * 完成任务
+     * @param id
+     */
+    void complete(String id);
+
+    /**
+     * 申请任务
+     * @param id
+     * @param userId
+     */
+    void receive(String id,String userId);
 }
